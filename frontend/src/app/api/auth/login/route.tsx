6f6b93a1f06a33e60f45ac98483431c07213a991
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-06-16 13:20:00 -0300

    feat: update authentication routes to use environment variable for API URL

--- frontend/src/app/api/auth/login/route.tsx
+++ frontend/src/app/api/auth/login/route.tsx
@@ -1,8 +1,9 @@
 import { NextResponse } from "next/server";
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
 export async function POST(req: Request) {
   const body = await req.json();
-  const res = await fetch("http://localhost:4000/api/auth/login", {
+  const res = await fetch(`${apiUrl}/api/auth/login`, { 
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(body),
--- frontend/src/app/api/auth/register/route.tsx
+++ frontend/src/app/api/auth/register/route.tsx
@@ -1,4 +1,5 @@
 import { NextResponse } from "next/server";
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
 import type { NextRequest } from "next/server";
 
@@ -6,7 +7,7 @@ export async function POST(req: NextRequest) {
   const body = await req.json();
   // Aqui você pode chamar sua API backend, ou usar Prisma direto se preferir SSR
   // Exemplo chamando backend Express:
-  const res = await fetch("http://localhost:4000/api/auth/register", {
+  const res = await fetch(`${apiUrl}/api/auth/register`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(body),
